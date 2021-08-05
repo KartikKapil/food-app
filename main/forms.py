670e5e023f07d2524c4ffb3de7c9c10e215649e8
Kartik Kapil <kkapil58@gmail.com>
2021-08-05 20:05:57 +0530

    Add Vendor Model Complete Login Auth 2.0

--- main/forms.py
+++ main/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import fields
 
-from .models import Document, Student
+from .models import *
 
 
 class NewUserForm(UserCreationForm):
@@ -23,5 +23,9 @@ class DocumentForm(forms.Form):
     # title = forms.CharField(max_length=50)
     file = forms.FileField()
 
+class VendorForm(forms.ModelForm):
+    class Meta:
+        model = Vendor
+        fields = '__all__'
 
 
